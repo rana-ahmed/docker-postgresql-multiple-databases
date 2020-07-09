86f946238e9d5c2b1b7fb705b8a08e0764a46408
Minuddin Ahmed Rana <minuddinrana@gmail.com>
2020-07-09 21:12:42 +0600

    Fix

--- create-multiple-postgresql-databases.sh
+++ create-multiple-postgresql-databases.sh
@@ -7,7 +7,7 @@ function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE DATABASE IF NOT EXISTS $database;
+	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 	    \connect $database;
       CREATE SCHEMA IF NOT EXISTS $DB_SCHEMA;
